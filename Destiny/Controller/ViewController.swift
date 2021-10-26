308c799381d6142f371b5107cf4171607530280a
Yunus Gedik <yunus-gedik@outlook.com>
2021-10-26 23:18:14 +0300

    feat: app is complete

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -1,20 +1,29 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-
+   
+   let brain = StoryBrain();
+   
+   @IBOutlet weak var firstButton: UIButton!
+   @IBOutlet weak var secondButton: UIButton!
+   
+   @IBOutlet weak var question: UILabel!
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      updateUI()
+   }
+   
+   @IBAction func buttonClicked(_ sender: UIButton) {
+      brain.setDestination(sender.currentTitle!)
+      Timer.scheduledTimer(timeInterval: 0, target: self, selector: #selector(updateUI) , userInfo: nil, repeats: false)
+   }
+   
+   @objc func updateUI(){
+      question.text = brain.getTitle();
+      firstButton.setTitle(brain.getFirstChoice(), for: .normal)
+      secondButton.setTitle(brain.getSecondChoice(), for: .normal)
+   }
+   
 }
 
